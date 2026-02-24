767beee1bd67b20ae5e7adcc030fde3122ecd33c
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-02-24 13:51:02 -0800

    first time trying tailwind

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,22 +1,28 @@
 import { useState } from "react";
 import styles from './Navbar.module.css';
+import { current } from "@reduxjs/toolkit";
 
 const pages = ['Standings', 'Teams', 'Players', 'Head to Head Games']
 
 export default function Navbar ({currentPage, onNavigate}) {
     return (
-        <nav className={styles.navbar}>
-        <div className={styles.logo}>
-          <span className={styles.logoIcon}>⚾</span>
-          <span className={styles.logoText}>MLB Analytics</span>
+        <nav className="flex items-cetner justify-between bg-blue-950 px-8 h-16 border-b-4 border-black sticky">
+        <div className = "flex items-cetner gap-2 text-white text-xl">
+          <span className="text-2xl">⚾</span>
+          <span className="font-semibold">MLB Analytics</span>
         </div>
 
-        <ul className={styles.navLinks}>
+        <ul className="flex gap-2 list-none">
         {pages.map(page => (
           <li key={page}>
             <button
-              className={`${styles.navBtn} ${currentPage === page ? styles.active : ''}`}
+              
               onClick={() => onNavigate(page)}
+              className={`px-4 py-2 rounded text-sm text-white
+                ${currentPage === page
+                  ? 'bg-blue-50 font-semibold'
+                  : 'opacity-75'
+                }`}
             >
               {page}
             </button>
--- src/pages/StandingsPage.jsx
+++ src/pages/StandingsPage.jsx
@@ -1,3 +1,3 @@
 export default function StandingsPage() {
-    return <div style={{ padding: '2rem' }}>Teams Page</div>;
+    return <div style={{ padding: '2rem' }}>Standings Page</div>;
   }
