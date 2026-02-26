29512274e51eee3a6ca5decdeac82ecbed0c2d93
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-02-26 11:16:39 -0800

    got the top 5 teams in the standings in console, now time to render

--- src/pages/StandingsPage.jsx
+++ src/pages/StandingsPage.jsx
@@ -3,10 +3,35 @@ import {fetchStandings} from '../services/mlbAPI';
 
 
 
+export default function StandingsPage(){
 
-export default function StandingsPage() {
-    return (
-      <div style={{ padding: '2rem' }}>Standings Page</div>;
-    );
-    
-  }
+
+  const [teams, setTeams] = useState([]);
+
+
+  useEffect(()=>{
+    fetchStandings(2025).then(data=>{
+      const first = data.records[0].teamRecords;
+      const top5 = first.slice(0,5).map(t=>({
+        name: t.team.name,
+        wins:t.wins,
+        losses:t.losses,
+      }));
+      console.log(top5);
+      setTeams(top5);
+    })
+  }, [])
+
+
+
+
+
+  return(
+    <div>
+      <h1>2025 MLB Standings</h1>
+      <div>
+        <p>Check Console</p>
+      </div>
+    </div>
+  )
+}
