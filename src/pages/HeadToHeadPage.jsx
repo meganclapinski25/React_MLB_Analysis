827be54c8f9c3e5ffabb876fd0d0697115c22eed
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-02-26 13:59:30 -0800

    starting head to head page frontend

--- src/pages/HeadToHeadPage.jsx
+++ src/pages/HeadToHeadPage.jsx
@@ -1,3 +1,11 @@
+
+
+
 export default function HeadToHeadPage() {
-    return <div style={{ padding: '2rem' }}>Head to Head Page</div>;
+    return (
+      <div className="p-8">
+        <h1 className="text-2xl font-bold text-white mb-2">Head to Head</h1>
+        <p className="text-grey-400 mb-6">Is this team's record real or flulky *change this later**</p>
+      </div>
+    )
   }
