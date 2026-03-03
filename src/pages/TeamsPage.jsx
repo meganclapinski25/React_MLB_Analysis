54757f2d06b46828f30c7590f435749dfb00ca6e
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-03-03 11:55:49 -0800

    adding temp frontend

--- src/pages/TeamsPage.jsx
+++ src/pages/TeamsPage.jsx
@@ -1,5 +1,19 @@
 import { useState } from 'react';
 
 export default function TeamsPage() {
-    return <div style={{ padding: '2rem' }}>Teams Page</div>;
+
+  const [selectedTeam, setSelectedTeam] = usState(null);
+
+
+    return (
+      <div className='flex flex-col items-center p-8'>
+        <h1 className='"text-2xl font-bold'>Teams</h1>
+        <p className='text-grey-600 mb-6'>Analyictical Question</p>
+
+
+
+
+
+      </div>
+    )
   }
