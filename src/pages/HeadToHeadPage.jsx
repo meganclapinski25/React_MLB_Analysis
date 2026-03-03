cb708c267a58cd5899f6df810e1b57d494dd004d
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-03-03 10:28:32 -0800

    trying to finsihe head to head page

--- src/pages/HeadToHeadPage.jsx
+++ src/pages/HeadToHeadPage.jsx
@@ -2,6 +2,27 @@ import { useState } from 'react';
 import { fetchStandings } from '../services/mlbAPI';
 
 export default function HeadToHeadPage() {
+
+  const [teamA, setTeamA] = useState(null);
+  const [teamB, setTeamB] = useState(null);
+
+    function handleTeamA(e){
+      const selectedId = parseInt(e.target.value);
+      const selectedTeam = teams.find(t=>t.id === selectedId);
+      setTeamA(selectedTeam);
+    }
+
+    function handleTeamB(e){
+      const selectedId = parseInt(e.target.value);
+      const selectedTeam = teams.find(t=>t.id === selectedId);
+      setTeamB(selectedTeam);
+    }
+
+
+
+
+
+
     return (
       <div className="p-8">
         <h1 className="text-2xl font-bold text-white mb-2">Head to Head</h1>
