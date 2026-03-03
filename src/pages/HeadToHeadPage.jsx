d616053b744ac1541d9ec4ead9bc12eca466abcb
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-03-03 10:31:20 -0800

    added front end

--- src/pages/HeadToHeadPage.jsx
+++ src/pages/HeadToHeadPage.jsx
@@ -27,6 +27,22 @@ export default function HeadToHeadPage() {
       <div className="p-8">
         <h1 className="text-2xl font-bold text-white mb-2">Head to Head</h1>
         <p className="text-grey-400 mb-6">Is this team's record real or flulky *change this later**</p>
+      
+        <div className='flex gap-6 mb-8'>
+          <select defaultValue="" onChange={handleTeamA}>
+            <option value="" disabled>Select Team A...</option>
+            {teams.map(t => <option key={t.id} value={t.id}>{t.name}</option>)}
+          </select>
+
+          <span>vs</span>
+
+          <select defaultValue="" onChange={handleTeamB}>
+            <option value="" disabled>Select Team B...</option>
+            {teams.map(t => <option key={t.id} value={t.id}>{t.name}</option>)}
+          </select>
+
+
+        </div>
       </div>
     );
   }
