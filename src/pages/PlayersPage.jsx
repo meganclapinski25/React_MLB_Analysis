0d029aeb9c63b32c62fff7918ea456d9b9f63317
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-03-03 12:22:01 -0800

    did the front end of the players page no data yet

--- src/pages/PlayersPage.jsx
+++ src/pages/PlayersPage.jsx
@@ -1,9 +1,39 @@
-import { useState } from react;
+import { useState } from 'react';
 
 
 
 
 
 export default function PlayersPage() {
-    
+    const [query, setQuery] = useState('');
+    const [searched, setSearched] = useState(false);
+
+
+    function handleSearch(e){
+      e.preventDefault();
+      setSearched(true);
+    }
+
+    return (
+      <div className='flex flex-col items-center p-8'>
+        <h1 className='text-2xl font-bold text-white mb-2'> Players </h1>
+        <p className='text-grey-400 mb-6'> Analyical Question</p>
+
+        <form onSubmit={handleSearch} className="flex gap-3 mb-8">
+        <input
+          type="text"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          placeholder="Search player name"
+          className="bg-gray-800 text-white px-4 py-2 rounded-lg border border-gray-600 focus:outline-none w-72"
+        />
+        <button
+          type="submit"
+          className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg"
+        >
+          Search
+        </button>
+      </form>
+      </div>
+    )
   }
