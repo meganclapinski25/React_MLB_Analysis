4305bdd27b1a12c45c8049add847a0f536e81e3f
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2026-03-05 08:35:07 -0800

    added a visualization through rechart

--- src/pages/HeadToHeadPage.jsx
+++ src/pages/HeadToHeadPage.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { useGetStandingsQuery } from '../features/mlb/mlbApi';
 import StatCard from '../components/shared/StatCard';
 import TeamSelect from '../components/shared/TeamSelect';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 export default function HeadToHeadPage() {
 
@@ -17,6 +19,7 @@ export default function HeadToHeadPage() {
           id:t.team.id,
           name: t.team.name,
           wins: t.wins,
+          winPct: parseFloat(t.winningPercentage),
         }))
       ) ?? [];
      
@@ -40,7 +43,7 @@ export default function HeadToHeadPage() {
     return (
       <div className="p-8">
         <h1 className="text-2xl font-bold text-white mb-2">Head to Head</h1>
-        <p className="text-grey-400 mb-6">Analyitcal Question</p>
+        <p className="text-grey-400 mb-6">Which team has a better win percentage this season?</p>
       
         <div className='flex flex-col mb-8 items-center  '>
           <TeamSelect teams={allTeams} onChange={handleTeamA} placeholder="Select Team A" />
@@ -61,6 +64,27 @@ export default function HeadToHeadPage() {
             <StatCard label="Actual Wins" value={teamB?.wins} />
           </div>
         </div>
+        {teamA && teamB && (
+          <div className="mt-8">
+            <h2 className="text-white text-xl font-bold mb-4">Win % Comparison</h2>
+            <BarChart
+              style={{ width: '100%', maxWidth: '700px', aspectRatio: 1.618 }}
+              responsive
+              data={[
+                { name: teamA.name, winPct: teamA.winPct },
+                { name: teamB.name, winPct: teamB.winPct },
+              ]}
+              margin={{ top: 5, right: 0, left: 0, bottom: 5 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" stroke="#9CA3AF" />
+              <YAxis domain={[0, 1]} tickFormatter={v => `${(v * 100).toFixed(0)}%`} stroke="#9CA3AF" />
+              <Tooltip formatter={v => `${(v * 100).toFixed(1)}%`} />
+              <Legend />
+              <Bar dataKey="winPct" fill="#3B82F6" radius={[10, 10, 0, 0]} />
+            </BarChart>
+          </div>
+        )}
       </div>
     );
   }
